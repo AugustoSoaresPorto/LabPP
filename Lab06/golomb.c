a2cfb9a1112be9dd929bec2f05f212e4ccc033a8
AugustoSoaresPorto <115162034+AugustoSoaresPorto@users.noreply.github.com>
2023-01-15 19:29:02 -0300

    Add files via upload

--- Lab06/golomb.c
+++ Lab06/golomb.c
@@ -5,32 +5,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int golomb(int * listaGolomb, int intervalo);
+int golomb(int n); 
+double media(int * vetor, int inicio, int final);
 
 int main() {
-    int a=0,b=0, aux=0, intervalo=0;
+    int a=0,b=0, aux=0;
     int * listaGolomb;
+
     printf("Informe os valores de a e b:\n");
     scanf("%d %d", &a,&b);
     if(b < a) { aux = a; a = b; b = a; }
-    intervalo = b-a+1;
 
-    listaGolomb = (int *) malloc(sizeof(int)*(intervalo));
+    listaGolomb = (int *) malloc(sizeof(int)*(b+1));
 
-    golomb(listaGolomb, intervalo);
+    for (int i=0; i!=b+1; i++)
+        listaGolomb[i] = golomb(i);
     
-    for(int i=0; i!=intervalo; i++) {
-        printf("%d ", listaGolomb[i]);
-    }
+//    for(int i=0; i!=b+1; i++) {
+//        printf("%d ", listaGolomb[i]);
+//    }
 
+    printf("O primeiro elemento e: %d\nO ultimo elemento e: %d\n", listaGolomb[a],listaGolomb[b]);
+    printf("A media dos valores dos elementos e: %lg", media(listaGolomb, a, b+1));
 }
 
-int golomb(int * listaGolomb, int intervalo) {
-    for (int i=0; i!=intervalo; i++){
-        if (i=0)
-            listaGolomb[i] = 1;
-        else {
-            listaGolomb[i] = 1+(golomb(listaGolomb, (i -golomb(listaGolomb, (golomb(listaGolomb,(i-1))-1)))));
-        }
+int golomb(int i) {
+    if (i==0)
+        return 1;
+    else {
+        return (1 + golomb(i - golomb(golomb(i-1) - 1)));
     }
+}
+
+
+double media(int * vetor, int inicio, int final) {
+    double soma = 0;
+    for (int i=inicio; i!=final; i++) {
+        soma += vetor[i];
+    }
+    return soma/(final-inicio);
 }
